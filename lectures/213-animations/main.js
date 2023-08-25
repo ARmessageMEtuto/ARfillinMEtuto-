bdc46c781522f0b6c4fec136725688cb0cf4c70b
ARmessageMEtuto <125932630+ARmessageMEtuto@users.noreply.github.com>
2023-08-25 00:14:07 -0500

    Update main.js
    yes

--- lectures/213-animations/main.js
+++ lectures/213-animations/main.js
@@ -13,7 +13,7 @@ document.addEventListener('DOMContentLoaded', () => {
     scene.add(light);
 
     const gltf = await loadGLTF('../../assets/models/musicband-raccoon/scene.gltf');
-    gltf.scene.scale.set(0.1, 0.1, 0.1);
+    gltf.scene.scale.set(0.90, 0.90, 0.90);
     gltf.scene.position.set(0, -0.4, 0);
 
     const anchor = mindarThree.addAnchor(0);
